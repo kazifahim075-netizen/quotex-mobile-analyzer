22be7be530618466a82732018c87dc796cd57b78
kazifahim075-netizen <kazifahim075@gmail.com>
2026-08-31 01:33:38 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,28 @@
+chrome.action.onClicked.addListener(async (tab) => {
+  if (!tab.id) return;
 
+  chrome.tabs.sendMessage(tab.id, {
+    type: "TOGGLE_PANEL"
+  }).catch(() => {});
+});
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg?.type === "CAPTURE_VISIBLE") {
+    chrome.tabs.captureVisibleTab(
+      sender.tab.windowId,
+      {
+        format: "jpeg",
+        quality: 70
+      },
+      (dataUrl) => {
+        sendResponse({
+          ok: !chrome.runtime.lastError,
+          dataUrl,
+          error: chrome.runtime.lastError?.message
+        });
+      }
+    );
+
+    return true;
+  }
+});
